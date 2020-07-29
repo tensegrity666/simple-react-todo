d258947ee608cda72132ed677181382fb3074e26
Roman Yakovlev <tensegrity666@icloud.com>
2020-07-29 23:11:44 +0300

    init: start project

---
+++ src/components/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import style from './index.module.css';
+
+function App() {
+  return (
+    <div className={ style.app }>
+      <header className={ style.header }>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
