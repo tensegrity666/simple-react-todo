134299da31f6ecc0e35ee07eb8edf20834374cb5
Roman Yakovlev <tensegrity666@eicloud.com>
2020-08-05 00:32:09 +0300

    feat: implement item deleting

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+
+import React, { Component } from 'react';
 
 import Header from '../header';
 import TaskList from '../task-list';
@@ -7,15 +9,38 @@ import AddItemPanel from '../add-item-panel';
 
 import taskExamples from './constants';
 
-const App = () => {
-  return (
-    <main className="container">
-      <Header />
-      <FilterPanel />
-      <TaskList todos={taskExamples} onDeleted={(id) => console.log(id)} />
-      <AddItemPanel />
-    </main>
-  );
+class App extends Component {
+  state = {
+    data: taskExamples,
+  }
+
+  deleteTask = (id) => {
+    this.setState(({ data }) => {
+      const index = data.findIndex((el) => el.id === id);
+
+      const beforeIndex = data.slice(0, index);
+      const afterIndex = data.slice(index + 1);
+
+      const newData = [...beforeIndex, ...afterIndex];
+
+      return {
+        data: newData,
+      }
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <main className="container">
+        <Header />
+        <FilterPanel />
+        <TaskList todos={data} onDeleted={this.deleteTask} />
+        <AddItemPanel />
+      </main>
+    );
+  }
 };
 
 export default App;
