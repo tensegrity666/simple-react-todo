444d3ec773f912878e683c258b7f5fb6831094e2
Roman Yakovlev <tensegrity666@eicloud.com>
2020-08-04 23:20:50 +0300

    feat: add interactions with list items

--- src/components/task-list-item/index.js
+++ src/components/task-list-item/index.js
@@ -1,21 +1,53 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/state-in-constructor */
+
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { item } from './index.module.css';
+import { item, importantItem, completedItem, defaultItem } from './index.module.css';
 
 import ButtonGroup from '../button-group';
 
-const TaskListItem = ({ label }) => {
-  return (
-    <div className={item}>
-      <span>{label}</span>
-      <ButtonGroup />
-    </div>
-  );
-};
+class TaskListItem extends Component {
+  state = {
+    important: false,
+    done: false,
+  }
+
+  onLabelClick = () => {
+    this.setState(({ done }) => {
+      return {
+        done: !done,
+      }
+    });
+  }
+
+  render() {
+    const { label, onDeleted } = this.props;
+    const { important, done } = this.state;
+
+    return (
+      <div className={item}>
+        <span
+        className={done ? completedItem : important ? importantItem : defaultItem}
+          onClick={this.onLabelClick} onKeyPress={this.onLabelClick} role="listitem" tabIndex="0" >
+          {label}
+        </span>
+        <ButtonGroup onDeleted={onDeleted}/>
+      </div>
+    );
+  }
+}
 
 TaskListItem.propTypes = {
   label: PropTypes.string.isRequired,
+  onDeleted: PropTypes.func,
+};
+
+TaskListItem.defaultProps = {
+  onDeleted: () => {},
 };
 
 export default TaskListItem;
--- src/components/task-list/index.js
+++ src/components/task-list/index.js
@@ -5,20 +5,34 @@ import PropTypes from 'prop-types';
 
 import TaskListItem from '../task-list-item';
 
-const TaskList = ({ todos }) => {
+const TaskList = ({ todos, onDeleted }) => {
   const items = todos.map((item) => {
+    const { id, label } = item;
+
     return (
-      <li className="list-group-item">
-        <TaskListItem label={item.label} important={item.important} />
+      <li key={id} className="list-group-item">
+        <TaskListItem
+          label={label}
+          onDeleted={()=> onDeleted(id)} />
       </li>
     );
   });
 
-  return <ul className="list-group">{items}</ul>;
+  return (
+    <>
+      <h3 className="visually-hidden">tasks</h3>
+      <ul className="list-group">{items}</ul>
+    </>
+  );
 };
 
 TaskList.propTypes = {
   todos: PropTypes.array.isRequired,
+  onDeleted: PropTypes.func,
+};
+
+TaskList.defaultProps = {
+  onDeleted: () => {},
 };
 
 export default TaskList;
